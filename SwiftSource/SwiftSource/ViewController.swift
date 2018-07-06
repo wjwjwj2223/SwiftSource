c32fc6be3fdd8d2191ef9b68211549f5b93dbd1a
wangjie <17054214@qq.com>
2018-07-06 11:12:24 +0800

    init

---
+++ SwiftSource/SwiftSource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSource
+//
+//  Created by 王杰 on 2018/7/6.
+//  Copyright © 2018 王杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
